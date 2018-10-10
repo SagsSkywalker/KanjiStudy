df01804d6bb5400eb0d5ca915f5b18055b004e56
Sergio Guillen <sagssky@gmail.com>
2018-10-09 23:48:07 -0500

    Euclidian 3D

--- kmeans-proyecto/ImageCluster/images.py
+++ kmeans-proyecto/ImageCluster/images.py
@@ -1,12 +1,19 @@
 from PIL import Image, ImageOps
 from numpy import array
+import math
+
+def Euclidian3d(d1,d2):
+    return math.sqrt(math.pow(d1[0]-d2[0],2)+math.pow(d1[1]-d2[1],2)+math.pow(d1[2]-d2[2],2))
+
 image1 = Image.open('coke.jpg')
 image1.load()
-r, g, b, a = image1.split()
-image1 = Image.merge('RGB',(r,g,b))
+# r, g, b, a = image1.split()
+# image1 = Image.merge('RGB',(r,g,b))
 arr = array(image1)
 # image1 = ImageOps.posterize(image1,8)
 # print(arr[1])
-for x in arr:
-    print(x)
+# for x in arr:
+#     print(x)
+# print(arr[0][0],arr[51][0])
+print(Euclidian3d(arr[0][0],arr[51][0]))
 # image1.show()
