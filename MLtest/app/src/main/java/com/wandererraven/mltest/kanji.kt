5a7c554c26aaeb99a731558151489118e0472f0c
Sergio Guillen <sagssky@gmail.com>
2018-11-17 19:18:24 -0600

    Android Project with Kanji Adapter List

---
+++ MLtest/app/src/main/java/com/wandererraven/mltest/kanji.kt
@@ -0,0 +1,77 @@
+package com.wandererraven.mltest
+
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class KanjiListElement(): Parcelable{
+    var kunyomi: String = ""
+    var onyomi: String = ""
+    var meaning: String = ""
+    var image: Int = R.mipmap.selectKanji
+
+    constructor(parcel: Parcel) : this() {
+        kunyomi = parcel.readString()
+        onyomi = parcel.readString()
+        meaning = parcel.readString()
+        image = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(kunyomi)
+        parcel.writeString(onyomi)
+        parcel.writeString(meaning)
+        parcel.writeInt(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<KanjiListElement> {
+        override fun createFromParcel(parcel: Parcel): KanjiListElement {
+            return KanjiListElement(parcel)
+        }
+
+        override fun newArray(size: Int): Array<KanjiListElement?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
+
+class KanjiListAdapter(private val context: Context, private val dataSource: ArrayList<KanjiListElement>): BaseAdapter(){
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = inflater.inflate(R.layout.kanji_element, parent, false)
+        //Declarar vistas
+        val image = rowView.findViewById(R.id.kanji_element_image) as ImageView
+        val meaning = rowView.findViewById(R.id.kanji_element_meaning) as TextView
+        val kunyomi = rowView.findViewById(R.id.kanji_element_kunyomi) as TextView
+        //Armar el Kanji Element
+        val kanjiElement = getItem(position) as KanjiListElement
+        image.id = kanjiElement.image
+        meaning.text = kanjiElement.meaning
+        kunyomi.text = kanjiElement.kunyomi
+        return rowView
+    }
+
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+}
