42fd98c322d9814066606db38fd5c6d1095f4516
Ezequiel S. Sandoval <exql.sldana.sndval96@gmail.com>
2018-12-03 20:41:17 -0600

    Se devuelve arreglo con objetos json y se modifica el calculo en el script de R. Se añadieron tambien las librerias que se usan a una carpeta para evitar problemas al ejecutar desde python. Se comentaron líneas innecesarias en el script de R.

--- Flask/RL_r.R
+++ Flask/RL_r.R
@@ -1,39 +1,48 @@
-.libPaths(c('C:/Users/Ezequiel/Documents/R/win-library/3.5',
-            'C:/Program Files/R/R-3.5.1/library'))
-library(caTools)
-library(pROC)
+.libPaths(c('./3.5'))
+#library(caTools)
+#library(pROC)
 library(rjson)
-kanji <- read.csv('C:/Users/Ezequiel/Desktop/Flask/kanji.csv')
+myArgs <- commandArgs(trailingOnly = TRUE)
+myArgs <- as.numeric(myArgs)
+kanji <- read.csv('kanji.csv')
+kanji$learnt[myArgs[1]] <- myArgs[2]
+write.csv(kanji, file = 'kanji.csv', row.names = FALSE)
 kanjin <- data.frame(kanji[,5:7])
 kanjin$type <- as.numeric(as.factor(kanjin$type))
 
 set.seed(25)
-sp <- sample.split(kanjin, SplitRatio = 0.7)
+#sp <- sample.split(kanjin, SplitRatio = 0.7)
 
-train <- subset(kanjin, sp == 'TRUE')
-test <- subset(kanjin, sp == 'FALSE')
+#train <- subset(kanjin, sp == 'TRUE')
+#test <- subset(kanjin, sp == 'FALSE')
 
-a <- roc(train$learnt~train$strokes)
-b <- roc(train$learnt~train$type)
-ci.auc(a)
-ci.auc(b)
+#a <- roc(train$learnt~train$strokes)
+#b <- roc(train$learnt~train$type)
+#ci.auc(a)
+#ci.auc(b)
 
-m1 <- glm(learnt ~ strokes, train, family = 'binomial')
-summary(m1)
-dev.res <- c(min(summary(m1)$deviance.resid),
-             median(summary(m1)$deviance.resid),
-             max(summary(m1)$deviance.resid))
-dev.res <- data.frame(dev.res)
+#m1 <- glm(learnt ~ strokes, train, family = 'binomial')
+m2 <- glm(learnt ~ strokes, kanji, family = 'binomial')
+#summary(m1)
+summary(m2)
+#dev.res <- c(min(summary(m1)$deviance.resid),
+#             median(summary(m1)$deviance.resid),
+#             max(summary(m1)$deviance.resid))
+#dev.res <- data.frame(dev.res)
 
-coefficients <- summary(m1)$coefficients
-coefficients <- data.frame(coefficients)
+#coefficients <- summary(m1)$coefficients
+#coefficients <- data.frame(coefficients)
 
-pred <- predict(m1, test, type = 'response')
-plot(round(pred), type = 'l')
-t <- table(test$learnt, round(pred))
-t <- data.frame(cbind(t[,1:2]))
-colnames(t) <- c('c1','c2')
-fail <- (1-sum(test$learnt == round(pred))/length(test$learnt))*100
-fail <- data.frame(fail)
-write(toJSON(c(fail,t, data.frame(round(pred)),dev.res, coefficients)), 'mvdata.json')
-
+#pred <- predict(m1, test, type = 'response')
+pred1 <- predict(m2, kanji, type = 'response')
+plot(round(pred1), type = 'l')
+#t <- table(test$learnt, round(pred))
+#t <- data.frame(cbind(t[,1:2]))
+#colnames(t) <- c('c1','c2')
+#fail <- (1-sum(test$learnt == round(pred))/length(test$learnt))*100
+#fail <- data.frame(fail)
+fail <- (1-sum(kanji$learnt == round(pred1))/length(kanji$learnt))*100
+fail
+#write(toJSON(c(fail,t, data.frame(round(pred)),dev.res, coefficients)), 'mvdata.json')
+kanji <- cbind(kanji, clasification = c(round(pred1)))
+write(toJSON(c(kanji)), 'mvdata.json')
--- Flask/rl_kanji.py
+++ Flask/rl_kanji.py
@@ -6,31 +6,48 @@ import subprocess
 #variables
 js = ''
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/send', methods = ['GET','POST'])
 def sendData():
 	try:
 		if (request.method == 'POST'):
-			print('POST')
+			args = [request.args.get('id'), request.args.get('learnt')]
 			command = 'Rscript'
 			path2script = 'RL_r.R'
-			cmd = [command, path2script]
+			cmd = [command, path2script] + args
 			js = request.get_json()
 			x = subprocess.check_call(cmd, shell=False)
 			return redirect('get')
 		else:
 			print('GET')
-			return render_template('temp.html')
+			return jsonify({'res': 'OK'})
 	except Exception as e:
 		return jsonify({'error': e})
 
 @app.route('/get', methods = ['GET'])
 def getData():
+	with open('kanji.json', encoding = 'utf-8-sig') as kan:
+		jsk = json.load(kan)
+		kan.close()
 	with open('mvdata.json') as json_data:
+		njs = []
 		js = json.load(json_data)
+		for i in range(len(js['clasification'])):
+			njs.append(
+			{
+				'kanji' : jsk[i]['kanji'],
+				'kun' : js['kun'][i],
+				'on' : js['on'][i],
+				'meaning' : js['meaning'][i],
+				'strokes' : js['strokes'][i],
+				'type' : js['type'][i],
+				'learnt' : js['learnt'][i],
+				'clasification' : js['clasification'][i]
+			})
 		json_data.close()
-	return jsonify(js)
+	return jsonify(njs)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 6999))
-	app.run(host = '192.168.0.254', port = port, debug = True)
+	app.run(port = port, debug = True)
